08b96af99b5e6a9bba89cffda3872f8fe3e0db51
avina <avina.bansal@gmail.com>
2017-07-19 11:55:40 -0500

    recommendation bot

--- P3 Recommendations.py
+++ P3 Recommendations.py
@@ -66,7 +66,7 @@ def processLikes(iLike):
 # Load the movie names data (u.item) with just columns 0 and 1 (id and name)
 # id is np.int, name is S128
 movieNames = np.loadtxt('./ml-100k/u.item', dtype={'names': ('id', 'name'),
-                                                      'formats': (np.int, 'S128')}, delimiter='|', usecols=(0,1)) # TODO replace 0 with the correct cod eto load the movie data
+                                                   'formats': (np.int, 'S128')}, delimiter='|', usecols=(0,1)) # TODO replace 0 with the correct cod eto load the movie data
 
 # Create a dictionary with the ids as keys and the names as the values
 # Google search for "python merge lists into dictionary"
@@ -76,15 +76,16 @@ movieDict = dict(zip(movieNames['id'], movieNames['name'])) # TODO replace 0 wit
 
 # Load the movie Data (u.data) with just columns 0, 1, and 2 (userID, movieID, rating) all are np.int
 movieData = np.loadtxt('./ml-100k/u.data', dtype={'names': ('userID', 'movieID', 'rating'),
-                                                  'formats': (np.int, np.int, np.int)}, delimiter = '\t', usecols = (0, 1, 2)) # TODO replace 0 with the correct cod eto load the movie data
+                                                  'formats': (np.int, np.int, np.int)}, delimiter="\t", usecols = (0, 1, 2)) # TODO replace 0 with the correct cod eto load the movie data
 
 print(movieData)
 print ("")
 print(movieNames)
 print ("")
 print(movieDict)
+print ("")
 
-exit(0) # Delete this after we finish phase 1, for now just get the data loaded
+ # Delete this after we finish phase 1, for now just get the data loaded
 
 ########################################################
 # Begin Phase 2
@@ -94,37 +95,63 @@ exit(0) # Delete this after we finish phase 1, for now just get the data loaded
 # This is non-ideal, pandas, scipy, or graphlib should be used here
 
 # Create a dictionary to hold our temporary ratings
-movieRatingTemp = 0 # TODO replace 0 with code for an empty dictionary
+movieRatingTemp = {}  # TODO replace 0 with code for an empty dictionary
 
 # TODO For every row in the movie data, add the rating to a list in the dictionary entry
 # for that movies ID (don't forget to initialize the dictionary entry)
 
+for row in movieData:
+    if row['movieID'] not in movieRatingTemp:
+        movieRatingTemp[row['movieID']] = []
+    movieRatingTemp[row['movieID']].append(row['rating'])
 
+print (movieRatingTemp)
+print ("")
 # Create an empty dictionary for movieRating and movieRatingCount
-movieRating = 0 # TODO replace 0 with code for an empty dictionary
-movieRatingCount = 0 # TODO replace 0 with code for an empty dictionary
+movieRating = {} # TODO replace 0 with code for an empty dictionary
+movieRatingCount = {} # TODO replace 0 with code for an empty dictionary
 
 # TODO Using numpy place the average rating for each movie in movieRating and the total number of ratings in movieRatingCount
 # Note: You will need a for loop to get each dictionary key
-
+for key in movieRatingTemp:
+    movieRating[key] = np.mean(movieRatingTemp[key])
+    movieRatingCount[key] = len(movieRatingTemp[key])
 
 # Get sorting ratings
 # https://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
 movieRatingS = sorted(movieRating.iteritems(), key=lambda (k,v): (v,k), reverse=True)
+print (movieRatingS)
+print ("")
 
 # Top 10 Movies
 print("Top Ten Movies:")
 # TODO Print the top 10 movies
 # It should print the number, title, id, rating and count of reviews for each movie
 # ie 2. Someone Else's America (1995) (ID: 1599) Rating: 5.0 Count: 1
+for i in range(11):
+    print (str(i+1) + ". " + str(movieNames[movieRatingS[i][0]][1]) + "(ID: " + str(movieRatingS[i][0]) + ") " + "Rating: " + \
+           str(movieRating[movieRatingS[i][0]]) + " Count: " + str(movieRatingCount[movieRatingS[i][0]]))
 
-
-# Top 10 Movies with at least 100 ratings    
+# Top 10 Movies with at least 100 ratings
 print("\n\nTop Ten movies with at least 100 ratings:")
 # TODO It should print the same thing, but this time all the movies should have over 100 ratings
 # The number should be the movie's absolute rank
 # ie (16. Close Shave, A (1995) (ID: 408) Rating: 4.49 Count: 112)
 # Number 16 is first in this list because it's the first movie with over 100 ratings
+
+moviesPrinted = 0
+i = 0
+
+while moviesPrinted != 10:
+    id = movieRatingS[i][0]
+    if movieRatingCount[id] >= 100:
+        print (str(i) + ". " + str(movieNames[movieRatingS[i][0]][1]) + "(ID: " + str(movieRatingS[i][0]) + ") " + "Rating: " + \
+        str(movieRating[movieRatingS[i][0]]) + " Count: " + str(movieRatingCount[movieRatingS[i][0]]))
+        moviesPrinted += 1
+    i += 1
+
+# (str(i) + ". " + str(movieNames[movieRatingS[i][0]][1]) + "(ID: " + str(movieRatingS[i][0]) + ") " + "Rating: " + \
+#           str(movieRating[movieRatingS[i][0]]) + " Count: " + str(movieRatingCount[movieRatingS[i][0]]))
 
 exit(0) # Remove this line after we finish phase 2
 
